bd62a510be35961240639e0ca78a65eb767ecf4a
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2024-09-11 16:51:55 -0400

    Update COPDQuestionnaire.js

--- components/COPDQuestionnaire.js
+++ components/COPDQuestionnaire.js
@@ -59,7 +59,7 @@ const COPDQuestionnaire = () => {
     e.preventDefault();
     const group = determineCOPDGroup(coughSound, symptoms, CATScore, exacerbations, hospitalVisits);
     setResult(`The patient falls into: ${group}`);
-    setDebug(`${coughSound} + ${symptoms} + ${CATScore} + ${exacerbations} + ${hospitalVisits}`);
+    //setDebug(`${coughSound} + ${symptoms} + ${CATScore} + ${exacerbations} + ${hospitalVisits}`);
     if(CATScore < 10 && exacerbations <= 1 && hospitalVisits === 0){
       setDebug(`true`);
     }
