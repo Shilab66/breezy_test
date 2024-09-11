f18f8b82d5375b9d3ec107ac15e23aa7beb9aef5
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2024-09-10 20:37:36 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import COPDQuestionnaire from '../components/COPDQuestionnaire';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the COPD Assessment</h1>
+      <COPDQuestionnaire />
+    </div>
+  );
+};
+
+export default Home;
