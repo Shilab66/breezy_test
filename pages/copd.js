0c11fd894cddb076eb76aa0cdcd1207044c80c10
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2024-09-17 18:18:25 -0400

    Create copd.js

---
+++ pages/copd.js
@@ -0,0 +1,12 @@
+import COPDQuestionnaire from '../components/COPDQuestionnaire';
+
+const COPDPage = () => {
+  return (
+    <div>
+      <h1>COPD Questionnaire</h1>
+      <COPDQuestionnaire />
+    </div>
+  );
+};
+
+export default COPDPage;
