e652c098cf249781c2610bc07d9d3d39a7c694ac
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2024-09-17 18:17:36 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,21 @@
-import Link from 'next/link';  // Assuming you're using Next.js
+import Link from 'next/link';
 
 const HomePage = () => {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Welcome to the Questionnaire Hub</h1>
-      <p>Please choose one of the following questionnaires:</p>
-
-      <div style={{ margin: '20px' }}>
-        <Link href="../components/COPDQuestionnaire">
-          <button style={buttonStyle}>COPD Questionnaire</button>
+    <div>
+      <h1>Questionnaire Hub</h1>
+      <div>
+        <Link href="/copd">
+          <button>COPD Questionnaire</button>
         </Link>
       </div>
-
-      <div style={{ margin: '20px' }}>
-        <Link href="../components/TinkermanQuestionnaire">
-          <button style={buttonStyle}>Tinkerman Questionnaire</button>
+      <div>
+        <Link href="/tinkerman">
+          <button>Tinkerman Questionnaire</button>
         </Link>
       </div>
     </div>
   );
-};
-
-// Style for the buttons
-const buttonStyle = {
-  padding: '15px 30px',
-  fontSize: '18px',
-  cursor: 'pointer',
-  backgroundColor: '#0070f3',
-  color: 'white',
-  border: 'none',
-  borderRadius: '5px',
-  transition: 'background-color 0.3s',
 };
 
 export default HomePage;
