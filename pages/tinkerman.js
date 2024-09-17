b67b98379fe16f2f0c418ad3c9986f2ef5987bc6
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2024-09-17 18:18:37 -0400

    Create tinkerman.js

---
+++ pages/tinkerman.js
@@ -0,0 +1,12 @@
+import TinkermanQuestionnaire from '../components/TinkermanQuestionnaire';
+
+const TinkermanPage = () => {
+  return (
+    <div>
+      <h1>Tinkerman Questionnaire</h1>
+      <TinkermanQuestionnaire />
+    </div>
+  );
+};
+
+export default TinkermanPage;
