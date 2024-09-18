898c6536b1846a54470fffcac8f8eb626d94e0b6
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2024-09-18 12:29:52 -0400

    Update copd.js

--- pages/copd.js
+++ pages/copd.js
@@ -1,1 +1,14 @@
 
+import COPDQuestionnaire from '../components/COPDQuestionnaire.js';
+
+const COPDPage = () => {
+  return (
+    <div>
+      <h1>COPD Questionnaire</h1>
+      <COPDQuestionnaire />
+    </div>
+  );
+};
+
+export default COPDPage;
+
