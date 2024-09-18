2ced5c39f518840bfa2966877286d1736b3baaf4
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2024-09-18 12:31:04 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,30 @@
 import Link from 'next/link';
 
+
 const HomePage = () => {
-    <div>
-        <Link href="/audio">
-          <button>Audio Upload</button>
-        </Link>
-      </div>
+ return (
+   <div>
+     <h1>Questionnaire Hub</h1>
+     <div>
+       <Link href="/copd">
+         <button>COPD Questionnaire</button>
+       </Link>
+     </div>
+     <div>
+       <Link href="/tinkerman">
+         <button>Tinkerman Questionnaire</button>
+       </Link>
+     </div>
+
+
+   <div>
+       <Link href="/audio">
+         <button>Audio Upload</button>
+       </Link>
+     </div>
+   </div>
+ );
 };
+
 
 export default HomePage;
